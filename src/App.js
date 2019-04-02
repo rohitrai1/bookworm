df29f2ea407637bc8e28924bd3516ba832eddac2
rovit <rohit47099@gmail.com>
2019-04-02 19:58:22 +0530

    Implemented: React router setup with login page and homepage. [Bookworm-3]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HomePage from "./components/pages/Homepage";
+import LoginPage from "./components/pages/LoginPage";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/login" component={LoginPage} />
+      </div>
+    );
   }
 }
 
